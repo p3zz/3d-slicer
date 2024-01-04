7e1f5850a5559470d13b2027570fd15ea9a97580
p3zz <federico.pezzato.work@gmail.com>
2024-01-04 21:15:45 +0100

    [WIP] improve math functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # https://github.com/stephenyeargin/stl-files
 
-import vpython
+# import vpython
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import re
@@ -17,6 +17,9 @@ class Segment:
     
     def __eq__(self, value: object) -> bool:
         return (self.p == value.p and self.q == value.q) or (self.p == value.q and self.q == value.p)
+
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
     
     def __str__(self):
         return 'p: {} q: {} normal: {}\n'.format(self.p, self.q, self.normal)
@@ -40,20 +43,41 @@ class Polygon:
         return compute_centroid(self.points)
 
 def compute_centroid(points):
-    x = sum(p[0] for p in points) / len(points)
-    y = sum(p[1] for p in points) / len(points)
-    z = sum(p[2] for p in points) / len(points)
-    return np.array([x, y, z])
+    p = []
+    for i in range(len(points[0])):
+        c = sum(p[i] for p in points) / len(points)
+        p.append(c)
+    return np.array(p)
 
-def calculate_angle(point, centroid):
-    angle = atan2(point[1] - centroid[1], point[0] - centroid[0])
-    return angle % (2 * pi)
+def compute_magnitude(vec):
+    return np.sqrt(sum([c**2 for c in vec]))
+
+def compute_angle(vec):
+    return atan2(vec[1], vec[0])
+
+def compute_angle_from_center(point, center):
+    delta = point - center
+    return compute_angle(delta)
+
+def compute_angle_between(vec1, vec2):
+    n = np.dot(vec1, vec2)
+    d = compute_magnitude(vec1) * compute_magnitude(vec2)
+    return np.arccos(n/d)
+
+def mod2pi(angle: float) -> float:
+    angle = angle % (2*pi)
+    if angle < 0:
+        angle += (2*pi)
+    return angle
+
+def check_parallel(vec1, vec2) -> bool:
+    return np.cross(vec1, vec2) == 0
 
 def order_points_clockwise(points):
     if len(points) == 0:
         return []
     centroid = compute_centroid(points)
-    return sorted(points, key=lambda p: calculate_angle(p, centroid))
+    return sorted(points, key=lambda p: compute_angle_from_center(p, centroid))
 
 class ParserState:
     WaitingPoint = 0
@@ -101,9 +125,6 @@ def parse_stl(filename: str) -> list[Polygon]:
     file.close()
     return polys
 
-def flatten(list):
-    return [item for sublist in list for item in sublist]
-
 def intersect_segment_plane(edge: Segment, z_level: float):
     d = edge.get_displacement()
     # if dz=0 the edge is parallel to the plane
@@ -125,18 +146,17 @@ def intersect_polygon_plane(poly: Polygon, z_level: float):
     points = remove_duplicates(points)
     return order_points_clockwise(points)
 
-def draw_layer(layer: list[Segment]):
-    for segment in layer:
-        vpython.sphere(pos=vpython.vector(segment.p.x, segment.p.y, segment.p.z),radius=0.02)
-        vpython.sphere(pos=vpython.vector(segment.q.x, segment.q.y, segment.q.z),radius=0.02)
-        vpython.curve(vpython.vector(segment.p.x, segment.p.y, segment.p.z), vpython.vector(segment.q.x, segment.q.y, segment.q.z))
+# def draw_layer(layer: list[Segment]):
+#     for segment in layer:
+#         vpython.sphere(pos=vpython.vector(segment.p.x, segment.p.y, segment.p.z),radius=0.02)
+#         vpython.sphere(pos=vpython.vector(segment.q.x, segment.q.y, segment.q.z),radius=0.02)
+        # vpython.curve(vpython.vector(segment.p.x, segment.p.y, segment.p.z), vpython.vector(segment.q.x, segment.q.y, segment.q.z))
 
-def remove_duplicates(list):
-    no_duplicates_list = []
-    for elem in list:
-        if elem not in no_duplicates_list:
-            no_duplicates_list.append(elem)
-    return no_duplicates_list
+def remove_duplicates(l):
+    return list(set(l))
+
+def flatten(list):
+    return [item for sublist in list for item in sublist]
 
 # def merge_consecutive_parallel(s1: Segment, s2: Segment) -> Segment:
 #     if s1.q == s2.p:
@@ -151,53 +171,29 @@ def remove_duplicates(list):
 def check_consecutive(s1: Segment, s2: Segment) -> bool:
     return s1.q == s2.p or s1.p == s2.q or s1.q == s2.q or s1.p == s2.p
 
-def check_parallel(s1: Segment, s2: Segment) -> bool:
-    dis1 = s1.get_displacement()
-    dis2 = s2.get_displacement()
-    if(dis2.x != 0):
-        ratio = dis1.x / dis2.x
-    elif(dis2.y != 0):
-        ratio = dis1.y / dis2.y
-    elif(dis2.z != 0):
-        ratio = dis1.z / dis2.z
-    else:
-         return True
-    return global_round(dis2.x * ratio) == dis1.x and global_round(dis2.y * ratio) == dis1.y and global_round(dis2.z * ratio) == dis1.z
-
 def global_round(val: float) -> float:
     return round(val, 6)
 
-def mod2pi(angle: float) -> float:
-    angle = angle % (2*pi)
-    if angle<0:
-        return angle + (2*pi)
-    return angle
-
-def angle_between_segments(s1: Segment, s2: Segment):
-    a1 = mod2pi(atan2(s1.q.y - s1.p.y, s1.q.x - s1.p.x))
-    a2 = mod2pi(atan2(s2.q.y - s2.p.y, s2.q.x - s2.p.x))
-    return mod2pi(a2-a1)
-
 def main():
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print("STL file is missing")
         return
     filename = sys.argv[1]
     model = parse_stl(filename)
     layers = dict()
     model_edges = flatten([poly.get_edges() for poly in model])
-    model_z_values = flatten([[edge.p.z, edge.q.z] for edge in model_edges])
+    model_z_values = flatten([[edge.p[2], edge.q[2]] for edge in model_edges])
     min_z = min(model_z_values)
     max_z = max(model_z_values)
 
     # we need to create a layer every 0.1 mm (finest printing), so the step is 0.0001
-    non_optimized_layers = 0
-    optimized_layers = 0
+    # non_optimized_layers = 0
+    # optimized_layers = 0
     step = 0.05
     for z in np.arange(min_z, max_z, step):
         z = global_round(z)
         key = str(z)
-        layers[key] = []
+        # layers[key] = []
         layer_segments = []
         for poly in model:
             segments = intersect_polygon_plane(poly, z)
@@ -206,28 +202,28 @@ def main():
         print("Segments found: ",len(layer_segments))
         # non_optimized_layers+=len(layers[key])
         # print("Original Layer [{}] has {} edges".format(key, len(layers[key])))
-        try:
-            layers[key] = surfaces_from_segments(layer_segments)
-            print("Polygons found: ",len(layers[key]))
-            for surf in layers[key]:
-                print("len {} infill {}".format(len(surf.points), surf.fill))
-        except Exception as e:
-            print(e)
-            continue   
+        # try:
+        #     layers[key] = surfaces_from_segments(layer_segments)
+        #     print("Polygons found: ",len(layers[key]))
+        #     for surf in layers[key]:
+        #         print("len {} infill {}".format(len(surf.points), surf.fill))
+        # except Exception as e:
+        #     print(e)
+        #     continue   
     # print("Non optimized model contains {} segments".format(non_optimized_layers))
     # print("Optimized model layers {} segments".format(optimized_layers))
         
-    def update_chart(val):
-        keys = [key for key in layers.keys() if float(key)<=val]
-        for k in keys:
-            for p in layers[k]:
-                pass
-                # draw_layer(p.poly.get_edges())
+    # def update_chart(val):
+    #     keys = [key for key in layers.keys() if float(key)<=val]
+    #     for k in keys:
+    #         for p in layers[k]:
+    #             pass
+    #             # draw_layer(p.poly.get_edges())
 
-    vpython.canvas(width=1500, height=1500)
-    update_chart(max_z)
-    while True:
-        pass
+    # vpython.canvas(width=1500, height=1500)
+    # update_chart(max_z)
+    # while True:
+    #     pass
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
